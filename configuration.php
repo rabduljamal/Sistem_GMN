3744c0ef6aab58fc58e832aa683294e95d3233cd
r abdul jamal <admin@rjamal.id>
2017-09-22 14:24:16 +0700

    fix 404 not found

--- configuration.php
+++ configuration.php
@@ -1,13 +1,13 @@
 <?php
 
 	class SConfig{
-		var $_site_url = "http://localhost/Sistem_GMN/";
-		var $_document_root = "D:\Git\Sistem_GMN";
+		var $_site_url = "http://gmn.dev";
+		var $_document_root = "/var/www/html/GMN/";
 		var $_host_name = "localhost";
 		var $_site_name = "PT GLobal Media Nusantara";
 		var $_database_name = "sistem_gmn";
 		var $_database_user = "root";
-		var $_database_password = "";
+		var $_database_password = "481840";
 		var $_table_prefix = "tbl_";
 		var $_cms_name = "sistem_gmn";
 		var $_backend_perpage = 10;
